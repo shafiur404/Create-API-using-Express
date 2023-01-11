2bef0a37571acfbc5dc34ec730ada59d148d4709
shafiurdiu <shafiur35-1629@diu.edu.bd>
2023-01-11 20:06:14 +0600

    Added .env file and get using API

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 const PORT = "4000";
-app.get("/", (req, res) => {
-  res.send("Here I am");
+app.get("/razu", (req, res) => {
+    // console.log(process.env.PORT);
+  res.send({"Here I am": process.env.PORT});
 });
 app.listen(PORT, function (error) {
   if (error) {
